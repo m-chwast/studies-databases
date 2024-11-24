26218d6a04cdf48b87f400cf6cd2092a5ce7925f
Mateusz Chwast <chwastmat@gmail.com>
2024-11-24 21:00:37 +0100

    add logging in vm

--- project/app/AirlineManager/ViewModel/ConnectionViewModel.cs
+++ project/app/AirlineManager/ViewModel/ConnectionViewModel.cs
@@ -1,23 +1,28 @@
 using System.Reactive.Linq;
 using AirlineManager.Model;
 using ReactiveUI;
+using Microsoft.Extensions.Logging;
 
 namespace AirlineManager.ViewModel;
 
 public class ConnectionViewModel : ViewModelBase
 {
+    private readonly ILogger _logger;
     private ConnectionModel _connectionModel = new();
 
     private readonly ObservableAsPropertyHelper<string> _connectionStatus; 
     public string ConnectionStatus => _connectionStatus.Value;
 
-    public ConnectionViewModel()
+    public ConnectionViewModel(ILogger logger)
     {
+        _logger = logger;
+        
         _connectionStatus = this
             .WhenAnyValue(x => x._connectionModel.DatabaseConnectionState)
             .Select(status => status.ToString())
             .ToProperty(this, x => x.ConnectionStatus); 
 
+        _logger.LogInformation("Connecting to database");
         _connectionModel.Connect();
     }
 }
--- project/app/AirlineManager/ViewModel/MainWindowViewModel.cs
+++ project/app/AirlineManager/ViewModel/MainWindowViewModel.cs
@@ -1,16 +1,18 @@
-using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
 
 namespace AirlineManager.ViewModel;
 
 public class MainWindowViewModel : ViewModelBase
 {
-    private readonly List<ViewModelBase> _viewModels = [];
-
     private readonly ConnectionViewModel _connectionViewModel;
     public ConnectionViewModel ConnectionVM { get => _connectionViewModel; }
 
     public MainWindowViewModel()
     {
-        _connectionViewModel = new ConnectionViewModel();
+        using ILoggerFactory loggerFactory = LoggerFactory
+            .Create(builder => builder.AddConsole());
+        
+        _connectionViewModel = new ConnectionViewModel(
+            loggerFactory.CreateLogger("Connection"));
     }
 }
