4546443b093bc145605c7b5d9c5d5caefe94ee9f
Mateusz Chwast <chwastmat@gmail.com>
2024-11-24 14:38:06 +0100

    add connection user control

---
+++ project/app/AirlineManager/View/ConnectionView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace AirlineManager.View;
+
+public partial class ConnectionView : UserControl
+{
+    public ConnectionView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ project/app/AirlineManager/ViewModel/ConnectionViewModel.cs
@@ -0,0 +1,6 @@
+namespace AirlineManager.ViewModel;
+
+public class ConnectionViewModel
+{
+    
+}
