0898ebb7eb2acebad39b56a078ffd175ee44bb3b
Mateusz Chwast <chwastmat@gmail.com>
2025-01-11 17:04:41 +0100

    subscribe to refresh event in airline vm

--- project/app/AirlineManager/ViewModel/AircraftViewModel.cs
+++ project/app/AirlineManager/ViewModel/AircraftViewModel.cs
@@ -4,7 +4,15 @@ namespace AirlineManager.ViewModel;
 
 public class AircraftViewModel
 {
+    private IDatabase _database;
+
     public AircraftViewModel(IDatabase database)
+    {
+        _database = database;
+        _database.Refresh += (o,e) => Refresh();   
+    }
+
+    private void Refresh()
     {
         
     }
