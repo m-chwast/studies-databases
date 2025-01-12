c7b495dbfe492b94098e0c502a7cd115737e55ef
Mateusz Chwast <chwastmat@gmail.com>
2025-01-12 20:43:55 +0100

    add database stats text

--- project/app/AirlineManager/ViewModel/ConnectionViewModel.cs
+++ project/app/AirlineManager/ViewModel/ConnectionViewModel.cs
@@ -11,12 +11,20 @@ public class ConnectionViewModel : ViewModelBase
     private ObservableAsPropertyHelper<string> _connectionStatus;
     public string ConnectionStatus => _connectionStatus.Value;
 
+    private ObservableAsPropertyHelper<string> _databaseStats;
+    public string DatabaseStats => _databaseStats.Value;
+
     public ConnectionViewModel()
     {
         _connectionStatus = this
             .WhenAnyValue(x => x._connectionModel.DatabaseFault)
             .Select(x => x ? "Fault detected - refreshing..." : "OK")
             .ToProperty(this, x => x.ConnectionStatus);
+
+        _databaseStats = this
+            .WhenAnyValue(x => x._connectionModel)
+            .Select(x => "")
+            .ToProperty(this, x => x.DatabaseStats);
     }
 
     public IDatabase GetDatabase() => _connectionModel;
