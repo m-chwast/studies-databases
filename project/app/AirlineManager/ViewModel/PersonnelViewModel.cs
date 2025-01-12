b9188f76b260364e56ebbf89b651ac3e7794db2f
Mateusz Chwast <chwastmat@gmail.com>
2025-01-12 23:48:58 +0100

    refactor properties

--- project/app/AirlineManager/ViewModel/PersonnelViewModel.cs
+++ project/app/AirlineManager/ViewModel/PersonnelViewModel.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Reactive.Linq;
 using AirlineManager.Model;
 using ReactiveUI;
 
@@ -9,38 +11,27 @@ public class PersonnelViewModel : ViewModelBase
     public bool ShowFlightAttendants
     {
         get => _showFlightAttendants;
-        set
-        {
-            this.RaiseAndSetIfChanged(ref _showFlightAttendants, value, nameof(ShowFlightAttendants));
-            TriggerRefresh();
-        }
+        set => this.RaiseAndSetIfChanged(ref _showFlightAttendants, value, nameof(ShowFlightAttendants));
     }
 
     private bool _showCaptains = true;
     public bool ShowCaptains
     {
         get => _showCaptains;
-        set
-        {
-            this.RaiseAndSetIfChanged(ref _showCaptains, value, nameof(ShowCaptains));
-            TriggerRefresh();
-        }
+        set => this.RaiseAndSetIfChanged(ref _showCaptains, value, nameof(ShowCaptains));
     }   
 
     private bool _showFirstOfficers = true;
     public bool ShowFirstOfficers
     {
         get => _showFirstOfficers;
-        set
-        {
-            this.RaiseAndSetIfChanged(ref _showFirstOfficers, value, nameof(ShowFirstOfficers));
-            TriggerRefresh();
-        }
+        set => this.RaiseAndSetIfChanged(ref _showFirstOfficers, value, nameof(ShowFirstOfficers));
     }
-    
+
     public PersonnelViewModel(IDatabase database)
     {
-
+        this.WhenAnyValue(x => x.ShowFlightAttendants, x => x.ShowCaptains, x => x.ShowFirstOfficers)
+            .Subscribe(_ => TriggerRefresh());
     }
 
     private void TriggerRefresh()
