e2f3781dd321ea93c5efd3dbc5d81914f7ab6e93
Mateusz Chwast <chwastmat@gmail.com>
2025-01-12 23:37:47 +0100

    add boilerplate for controlling personnel display

--- project/app/AirlineManager/ViewModel/PersonnelViewModel.cs
+++ project/app/AirlineManager/ViewModel/PersonnelViewModel.cs
@@ -1,10 +1,49 @@
 using AirlineManager.Model;
+using ReactiveUI;
 
 namespace AirlineManager.ViewModel;
 
 public class PersonnelViewModel : ViewModelBase
 {
+    private bool _showFlightAttendants = true;
+    public bool ShowFlightAttendants
+    {
+        get => _showFlightAttendants;
+        set
+        {
+            this.RaiseAndSetIfChanged(ref _showFlightAttendants, value, nameof(ShowFlightAttendants));
+            TriggerRefresh();
+        }
+    }
+
+    private bool _showCaptains = true;
+    public bool ShowCaptains
+    {
+        get => _showCaptains;
+        set
+        {
+            this.RaiseAndSetIfChanged(ref _showCaptains, value, nameof(ShowCaptains));
+            TriggerRefresh();
+        }
+    }   
+
+    private bool _showFirstOfficers = true;
+    public bool ShowFirstOfficers
+    {
+        get => _showFirstOfficers;
+        set
+        {
+            this.RaiseAndSetIfChanged(ref _showFirstOfficers, value, nameof(ShowFirstOfficers));
+            TriggerRefresh();
+        }
+    }
+    
     public PersonnelViewModel(IDatabase database)
+    {
+
+    }
+
+    private void TriggerRefresh()
     {
 
     }
