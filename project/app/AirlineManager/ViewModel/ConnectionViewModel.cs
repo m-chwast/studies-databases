27179dbe694590b8265fec50e7656689c31f4f04
Mateusz Chwast <chwastmat@gmail.com>
2025-01-12 14:55:33 +0100

    add connection status info

--- project/app/AirlineManager/ViewModel/ConnectionViewModel.cs
+++ project/app/AirlineManager/ViewModel/ConnectionViewModel.cs
@@ -1,5 +1,7 @@
+using System.Reactive.Linq;
 using AirlineManager.Model;
 using Microsoft.Extensions.Logging;
+using ReactiveUI;
 
 namespace AirlineManager.ViewModel;
 
@@ -8,10 +10,17 @@ public class ConnectionViewModel : ViewModelBase
     private readonly ILogger _logger;
     private ConnectionModel _connectionModel = new();
 
+    private ObservableAsPropertyHelper<string> _connectionStatus;
+    public string ConnectionStatus => _connectionStatus.Value;
+
     public ConnectionViewModel(ILogger logger)
     {
         _logger = logger;
-        _logger.LogInformation("Connecting to database");
+        
+        _connectionStatus = this
+            .WhenAnyValue(x => x._connectionModel.DatabaseFault)
+            .Select(x => x ? "Fault detected - refreshing..." : "OK")
+            .ToProperty(this, x => x.ConnectionStatus);
     }
 
     public IDatabase GetDatabase() => _connectionModel;
