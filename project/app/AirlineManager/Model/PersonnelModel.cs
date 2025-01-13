733a1cff485c6b889dce2b18f6789856b4b30644
Mateusz Chwast <chwastmat@gmail.com>
2025-01-13 22:09:32 +0100

    implement querying personnel

--- project/app/AirlineManager/Model/PersonnelModel.cs
+++ project/app/AirlineManager/Model/PersonnelModel.cs
@@ -16,7 +16,45 @@ public class PersonnelModel : ModelBase
     {
         List<PersonnelData> personnel = new();
 
+        string query = GetQuery(flightAttendants, captains, firstOfficers);
+        var dataTable = await _database.GetData(query);
+
+        foreach(var row in dataTable.Data)
+        {
+            PersonnelData person = new(row[0], row[1], row[2], row[3]);
+            personnel.Add(person);
+        }
+
         return personnel;
+    }
+
+    private string GetQuery(bool flightAttendants, bool captains, bool firstOfficers)
+    {
+        string query = @"
+            SELECT p.person_id, p.person_name, p.person_surname, r.role_name
+            FROM airline.person p
+            JOIN airline.role r
+            ON p.person_role_id = r.role_id
+            WHERE ";
+
+        bool getAnyRoles = flightAttendants || captains || firstOfficers;
+
+        // initialize to no roles, it will make our life easier later
+        string whereCondition = "False";
+        if(getAnyRoles)
+        {
+            if(flightAttendants)
+                whereCondition += " OR r.role_name = 'Flight Attendant'";
+            if(captains)
+                whereCondition += " OR r.role_name = 'Captain'";
+            if(firstOfficers)
+                whereCondition += " OR r.role_name = 'First Officer'";
+        }
+
+        query += whereCondition;
+        query += " ORDER BY p.person_surname;";
+
+        return query;           
     }
 
 }
