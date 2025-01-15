5beb5f16adb59208837d5946f5b24810608a2266
Mateusz Chwast <chwastmat@gmail.com>
2025-01-15 22:23:06 +0100

    add model methods for getting filtered personnel

--- project/app/AirlineManager/Model/PersonnelModel.cs
+++ project/app/AirlineManager/Model/PersonnelModel.cs
@@ -14,17 +14,9 @@ public class PersonnelModel : ModelBase
 
     public async Task<IEnumerable<PersonnelData>> GetNewData(bool flightAttendants, bool captains, bool firstOfficers)
     {
-        List<PersonnelData> personnel = new();
-
         string query = GetPersonnelQuery(flightAttendants, captains, firstOfficers);
         var dataTable = await _database.GetData(query);
-
-        foreach(var row in dataTable.Data)
-        {
-            PersonnelData person = new(row[0], row[1], row[2], row[3]);
-            personnel.Add(person);
-        }
-
+        var personnel = ExtractPersonnelFromDataTable(dataTable);
         return personnel;
     }
 
@@ -44,6 +36,31 @@ public class PersonnelModel : ModelBase
         return roles;
     }
 
+    public async Task<IEnumerable<PersonnelData>> GetFilteredPersonnel(int? id, string? surname)
+    {
+        if(id is null && string.IsNullOrEmpty(surname))
+            return new List<PersonnelData>();
+
+        string query = GetFilteredPersonnelQuery(id, surname);
+        var dataTable = await _database.GetData(query);
+
+        var personnel = ExtractPersonnelFromDataTable(dataTable);
+        return personnel;
+    }       
+
+    private List<PersonnelData> ExtractPersonnelFromDataTable(DataTable dataTable)
+    {
+        List<PersonnelData> personnel = new();
+
+        foreach(var row in dataTable.Data)
+        {
+            PersonnelData person = new(row[0], row[1], row[2], row[3]);
+            personnel.Add(person);
+        }
+
+        return personnel;
+    }
+
     public async Task<bool> AddPerson(string name, string surname, string role)
     {
         string query = $@"
@@ -51,6 +68,26 @@ public class PersonnelModel : ModelBase
             VALUES ('{name}', '{surname}', (SELECT role_id FROM airline.role WHERE role_name = '{role}'));";
 
         return await _database.ExecuteQuery(query);
+    }
+
+    private string GetFilteredPersonnelQuery(int? id, string? surname)
+    {
+        string query = @"
+            SELECT p.person_id, p.person_name, p.person_surname, r.role_name
+            FROM airline.person p
+            JOIN airline.role r
+            ON p.person_role_id = r.role_id
+            WHERE ";
+
+        bool isSurnameFilter = !string.IsNullOrEmpty(surname);
+        if(id is not null)
+            query += $"p.person_id = {id}" + (isSurnameFilter ? " AND " : "");
+        if(isSurnameFilter)
+            query += $"p.person_surname LIKE {surname}%";
+
+        query += ";";
+
+        return query;
     }
 
     private string GetPersonnelQuery(bool flightAttendants, bool captains, bool firstOfficers)
