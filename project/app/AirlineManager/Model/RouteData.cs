b5020bf81d227ab20f1554115e544a1a6d114029
Mateusz Chwast <chwastmat@gmail.com>
2025-01-16 21:53:21 +0100

    add routeData class

---
+++ project/app/AirlineManager/Model/RouteData.cs
@@ -0,0 +1,19 @@
+namespace AirlineManager.Model;
+
+public class RouteData
+{
+    public int Id { get; }
+    public string Departure { get; }
+    public string Destination { get; }
+    public float FlightTime { get; }
+    public bool IsSelected { get; }
+
+    public RouteData(string id, string departure, string destination, string flightTime, bool isSelected = false)
+    {
+        Id = int.Parse(id);
+        Departure = departure;
+        Destination = destination;
+        FlightTime = float.Parse(flightTime);
+        IsSelected = isSelected;
+    }
+}
