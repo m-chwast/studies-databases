fbcedd2f8ec1e87c5ce82c7f34e6db4edf3fbee8
Mateusz Chwast <chwastmat@gmail.com>
2025-01-16 18:59:34 +0100

    modify ids of most tables to be auto-generated

--- project/sql-scripts/structure_creation.sql
+++ project/sql-scripts/structure_creation.sql
@@ -17,15 +17,15 @@ CREATE TABLE person(person_id int GENERATED ALWAYS AS IDENTITY, person_name varc
                     PRIMARY KEY (person_id),
                     FOREIGN KEY (person_role_id) REFERENCES role(role_id));
 
-CREATE TABLE airport (airport_id int, airport_designator char(4), airport_name varchar(100),
+CREATE TABLE airport (airport_id int GENERATED ALWAYS AS IDENTITY, airport_designator char(4), airport_name varchar(100),
                       PRIMARY KEY (airport_id));
 
-CREATE TABLE route(route_id int, flight_time float, departure_airport_id int, arrival_airport_id int,
+CREATE TABLE route(route_id int GENERATED ALWAYS AS IDENTITY, flight_time float, departure_airport_id int, arrival_airport_id int,
                    PRIMARY KEY (route_id),
                    FOREIGN KEY (departure_airport_id) REFERENCES airport(airport_id),
                    FOREIGN KEY (arrival_airport_id) REFERENCES airport(airport_id));
 
-CREATE TABLE flight(flight_id int, flight_date date, flight_start_time time,
+CREATE TABLE flight(flight_id int GENERATED ALWAYS AS IDENTITY, flight_date date, flight_start_time time,
                     route_id int, aircraft_id int, captain_id int,
                     first_officer_id int, cabin_crew_id int[6],
                     PRIMARY KEY (flight_id),
@@ -34,7 +34,7 @@ CREATE TABLE flight(flight_id int, flight_date date, flight_start_time time,
                     FOREIGN KEY (captain_id) REFERENCES person(person_id),
                     FOREIGN KEY (first_officer_id) REFERENCES person(person_id));
 
-CREATE TABLE flight_crew(flight_crew_id int, flight_id int, person_id int,
+CREATE TABLE flight_crew(flight_crew_id int GENERATED ALWAYS AS IDENTITY, flight_id int, person_id int,
                          PRIMARY KEY (flight_crew_id),
                          FOREIGN KEY (flight_id) REFERENCES flight(flight_id),
                          FOREIGN KEY (person_id) REFERENCES person(person_id));
