3714f82d03bc0262b4f3b3909a0b74c331c90706
Mateusz Chwast <chwastmat@gmail.com>
2025-01-16 21:59:15 +0100

    add routes view data table

--- project/app/AirlineManager/ViewModel/RoutesViewModel.cs
+++ project/app/AirlineManager/ViewModel/RoutesViewModel.cs
@@ -1,10 +1,19 @@
+using System.Collections.ObjectModel;
 using AirlineManager.Model;
+using ReactiveUI;
 
 namespace AirlineManager.ViewModel;
 
 public class RoutesViewModel : ViewModelBase
 {
     private RoutesModel _model;
+
+    private ObservableCollection<RouteData> _routes = new();
+    public ObservableCollection<RouteData> Routes
+    {
+        get => _routes;
+        private set => this.RaiseAndSetIfChanged(ref _routes, value, nameof(Routes));
+    }
 
     public RoutesViewModel(IDatabase database)
     {
