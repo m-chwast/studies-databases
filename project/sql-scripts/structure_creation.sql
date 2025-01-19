d22e8b50dad6c448335ebb3c0cf81f4084a8fb8f
Mateusz Chwast <chwastmat@gmail.com>
2025-01-20 00:33:39 +0100

    make schema creation fool-proof

--- project/sql-scripts/structure_creation.sql
+++ project/sql-scripts/structure_creation.sql
@@ -4,34 +4,34 @@
 CREATE SCHEMA linia;
 SET SEARCH_PATH to linia;
 
-CREATE TABLE typ_samolotu(typ_samolotu_id int, typ_samolotu_nazwa char(20),
+CREATE TABLE linia.typ_samolotu(typ_samolotu_id int, typ_samolotu_nazwa char(20),
                            PRIMARY KEY (typ_samolotu_id));
-CREATE TABLE samolot(samolot_id int, typ_samolotu_id int,
+CREATE TABLE linia.samolot(samolot_id int, typ_samolotu_id int,
                       PRIMARY KEY (samolot_id),
-                      FOREIGN KEY (typ_samolotu_id) REFERENCES typ_samolotu(typ_samolotu_id));
+                      FOREIGN KEY (typ_samolotu_id) REFERENCES linia.typ_samolotu(typ_samolotu_id));
 
-CREATE TABLE rola(rola_id int, rola_nazwa char(30),
+CREATE TABLE linia.rola(rola_id int, rola_nazwa char(30),
                   PRIMARY KEY (rola_id));
-CREATE TABLE osoba(osoba_id int GENERATED ALWAYS AS IDENTITY, osoba_imie varchar(30), osoba_nazwisko varchar(30),
+CREATE TABLE linia.osoba(osoba_id int GENERATED ALWAYS AS IDENTITY, osoba_imie varchar(30), osoba_nazwisko varchar(30),
                     osoba_rola_id int,
                     PRIMARY KEY (osoba_id),
-                    FOREIGN KEY (osoba_rola_id) REFERENCES rola(rola_id));
+                    FOREIGN KEY (osoba_rola_id) REFERENCES linia.rola(rola_id));
 
-CREATE TABLE lotnisko(lotnisko_id int GENERATED ALWAYS AS IDENTITY, lotnisko_kod char(4), lotnisko_nazwa varchar(100),
+CREATE TABLE linia.lotnisko(lotnisko_id int GENERATED ALWAYS AS IDENTITY, lotnisko_kod char(4), lotnisko_nazwa varchar(100),
                       PRIMARY KEY (lotnisko_id));
 
-CREATE TABLE trasa(trasa_id int GENERATED ALWAYS AS IDENTITY, czas_lotu float, odlot_id int, przylot_id int,
+CREATE TABLE linia.trasa(trasa_id int GENERATED ALWAYS AS IDENTITY, czas_lotu float, odlot_id int, przylot_id int,
                    PRIMARY KEY (trasa_id),
-                   FOREIGN KEY (odlot_id) REFERENCES lotnisko(lotnisko_id),
-                   FOREIGN KEY (przylot_id) REFERENCES lotnisko(lotnisko_id));
+                   FOREIGN KEY (odlot_id) REFERENCES linia.lotnisko(lotnisko_id),
+                   FOREIGN KEY (przylot_id) REFERENCES linia.lotnisko(lotnisko_id));
 
-CREATE TABLE lot(lot_id int GENERATED ALWAYS AS IDENTITY, lot_data date,
+CREATE TABLE linia.lot(lot_id int GENERATED ALWAYS AS IDENTITY, lot_data date,
                     trasa_id int, samolot_id int,
                     PRIMARY KEY (lot_id),
-                    FOREIGN KEY (trasa_id) REFERENCES trasa(trasa_id),
-                    FOREIGN KEY (samolot_id) REFERENCES samolot(samolot_id));
+                    FOREIGN KEY (trasa_id) REFERENCES linia.trasa(trasa_id),
+                    FOREIGN KEY (samolot_id) REFERENCES linia.samolot(samolot_id));
 
-CREATE TABLE lot_zaloga(lot_zaloga_id int GENERATED ALWAYS AS IDENTITY, lot_id int, osoba_id int,
+CREATE TABLE linia.lot_zaloga(lot_zaloga_id int GENERATED ALWAYS AS IDENTITY, lot_id int, osoba_id int,
                          PRIMARY KEY (lot_zaloga_id),
-                         FOREIGN KEY (lot_id) REFERENCES lot(lot_id),
-                         FOREIGN KEY (osoba_id) REFERENCES osoba(osoba_id));
+                         FOREIGN KEY (lot_id) REFERENCES linia.lot(lot_id),
+                         FOREIGN KEY (osoba_id) REFERENCES linia.osoba(osoba_id));
